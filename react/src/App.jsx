e9bea4025f88d4f4666908665fdd95c45e775f64
Alice Smith <alice.webage.com>
2025-06-25 13:08:01 -0400

    Made components react

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,3 +1,8 @@
+import Sock from "./components/Sock";
+import Footer from "./components/Footer";
+import Search from "./components/Search";
+import sock_data from "./assets/sock.json";
+
 function App() {
 
   return (
@@ -31,10 +36,7 @@ function App() {
                 <a className="nav-link disabled" aria-disabled="true">Disabled</a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <Search />
           </div>
         </div>
       </nav>
@@ -43,7 +45,10 @@ function App() {
         <div className="container-fluid">
           <div className="row">
             Both socks and space rockets 🚀 will take you to new heights, but only one will get cold feet!
-            <div className="card-container"></div>
+            <div className="card-container">
+              <Sock data={sock_data}/>
+            </div>
+            <Footer environment={"DEVELOPMENT"}/>
           </div>
         </div>
       </main>
---
+++ react/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = (prop) => {
+    return (
+        <footer className="text-muted">
+            <div><strong>{prop.environment}</strong></div>
+        </footer>
+    );
+};
+
+export default Footer;
